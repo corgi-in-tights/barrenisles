89cb1f730b0efc357afda8e4b1cbec55a22e931b
Xyndra <sammy@deutschergamingserver.de>
2021-07-27 11:50:33 +0200

    All wood items are there, however not vanilla obtainable

---
+++ src/main/java/ca/thecorgi/barrenisles/items/PalmButton.java
@@ -0,0 +1,11 @@
+package ca.thecorgi.barrenisles.items;
+
+import ca.thecorgi.barrenisles.setup.ModBlocks;
+import net.minecraft.item.BlockItem;
+
+public class PalmButton extends BlockItem {
+
+    public PalmButton(Properties p_i48527_2_) {
+        super(ModBlocks.PALMBUTTON, p_i48527_2_);
+    }
+}
--- src/main/java/ca/thecorgi/barrenisles/setup/ModItems.java
+++ src/main/java/ca/thecorgi/barrenisles/setup/ModItems.java
@@ -2,6 +2,8 @@ package ca.thecorgi.barrenisles.setup;
 
 import ca.thecorgi.barrenisles.Creative_Tab;
 import ca.thecorgi.barrenisles.items.*;
+import ca.thecorgi.barrenisles.setup.ModBlocks;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -14,6 +16,15 @@ public class ModItems {
     public static final RegistryObject<Item> PALMLOGITEM = Registration.ITEMS.register("palm_log", () -> new PalmLogItem(new Item.Properties().tab(creativeTab)));
     public static final RegistryObject<Item> PALMPLANKITEM = Registration.ITEMS.register("palm_planks", () -> new PalmPlanksItem(new Item.Properties().tab(creativeTab)));
     public static final RegistryObject<Item> PALMTRAPDOORITEM = Registration.ITEMS.register("palm_trapdoor", () -> new PalmTrapdoorItem(new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> PALMBUTTON = Registration.ITEMS.register("palm_button", () -> new PalmButton(new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> PALMPRESSUREPLATE = Registration.ITEMS.register("palm_pressure_plate", () -> new PalmPressurePlate(new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> A = Registration.ITEMS.register("palm_slab", () -> new BlockItem(ModBlocks.PALMSLAB, new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> B = Registration.ITEMS.register("palm_stairs", () -> new BlockItem(ModBlocks.PALMSTAIRS, new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> C = Registration.ITEMS.register("palm_fence_gate", () -> new BlockItem(ModBlocks.PALMFENCEGATE, new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> D = Registration.ITEMS.register("palm_fence", () -> new BlockItem(ModBlocks.PALMFENCE, new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> E = Registration.ITEMS.register("palm_wood", () -> new BlockItem(ModBlocks.PALMWOOD, new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> F = Registration.ITEMS.register("stripped_palm_log", () -> new BlockItem(ModBlocks.STRIPPEDPALMLOG, new Item.Properties().tab(creativeTab)));
+    public static final RegistryObject<Item> G = Registration.ITEMS.register("stripped_palm_wood", () -> new BlockItem(ModBlocks.STRIPPEDPALMWOOD, new Item.Properties().tab(creativeTab)));
     public static final RegistryObject<Item> DUNERAPTORFEATHER = Registration.ITEMS.register("dune_feather", () -> new DuneraptorFeather(new Item.Properties().tab(creativeTab)));
 
     public static void register() {};
